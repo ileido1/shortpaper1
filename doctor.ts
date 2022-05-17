b758b52efef9ccd65cbe37cc56699e39f45f6088
ileido1 <soycarlosluis862@gmail.com>
2022-05-16 22:21:02 -0400

    updates 3

--- doctor.ts
+++ doctor.ts
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import { Agenda_consulta } from "./agenda_consulta";
 import { gestion_paciente } from "./gestion_paciente";
 import { Especialidad } from "./especialidad";
 import { ubicacion_geografica } from "./ubicacion_geografica";
 import { Historia_Medica } from "./historia_medica";
 import { Usuario } from "./usuario";
-=======
-export class Usuario{};
-export class Especialidad{};
->>>>>>> 594f517be6829da3b27c52f0b7aab6811b8f843f
 
 export class Doctor  extends Usuario implements gestion_paciente, Agenda_consulta{
  
